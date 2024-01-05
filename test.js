736f938eb6a9b9f6ae8968a28ba0ec3808300415
atikur-24 <atikurrahman7223@gmail.com>
2024-01-05 23:16:07 +0600

    compare all js function and explore diffrent ways

--- test.js
+++ test.js
@@ -5,6 +5,14 @@ function learn(topic) {
   console.log(`I am learning ${topic}`);
 }
 
-learn("JavaScript");
+// learn("JavaScript");
 
-console.log(`I know ${lang}`);
+// console.log(`I know ${lang}`);
+
+// function
+
+const hello = () => ({
+  a: 5,
+  b: 6,
+});
+console.log(hello());

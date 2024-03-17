4e2023930682177c0d138123656e6ac16b33a9ca
atikur-24 <atikurrahman7223@gmail.com>
2024-03-17 17:58:11 +0600

    recap apply method in js

--- callApplyBind.js
+++ callApplyBind.js
@@ -10,7 +10,7 @@ const myCustomObj = {
         },
     },
 };
-// myCustomObj.anotherObj.value.call(myCustomObj)
+// myCustomObj.anotherObj.value.call(myCustomObj);
 
 const john = {
     name: "John Doe",
@@ -24,6 +24,48 @@ const david = {
     name: "David Thomson",
     dob: 1986,
 };
+// john.age(2018);
+// john.age.call(david, 2018);
 
-// john.age(2018)
-// john.age.call(david, 2018)
+const person = {
+    fullName: function (city, country) {
+        return this.firstName + " " + this.lastName + "," + city + "," + country;
+    },
+};
+const person1 = {
+    firstName: "John",
+    lastName: "Doe",
+};
+const person2 = {
+    firstName: "Mary",
+    lastName: "Doe",
+};
+// console.log(person.fullName.call(person2, "Dhaka", "Bangladesh"));
+
+/* --------- Apply Method ---------- */
+
+const anotherPerson = {
+    fullName: function (city, country) {
+        return this.firstName + " " + this.lastName + "," + city + "," + country;
+    },
+};
+
+const anotherPerson1 = {
+    firstName: "John",
+    lastName: "Doe",
+};
+// console.log(person.fullName.apply(anotherPerson1, ["Oslo", "Norway"]));
+
+const martinez = {
+    name: "Martinez Emy",
+    dob: 1996,
+    age: function (currentYear, msg) {
+        return msg + " " + this.name + " " + (currentYear - this.dob) + " year old";
+    },
+};
+
+const jack = {
+    name: "Jack Fernando",
+    dob: 1986,
+};
+// console.log(martinez.age.apply(jack, [2018, "This is"]));

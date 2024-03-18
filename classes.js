cecea085f082a6bbb1d38f27b9c342e17d9e6d12
atikur-24 <atikurrahman7223@gmail.com>
2024-03-18 13:07:29 +0600

    recap object constructor in js

--- classes.js
+++ classes.js
@@ -24,29 +24,3 @@ class Car {
 
 const myCar = new Car("Ford", 2014);
 // console.log("My car is " + myCar.age() + " years old.");
-
-const Person = function (name, age, job) {
-    this.name = name;
-    this.age = age;
-    this.job = job;
-};
-
-const azad = new Person("Azad Ali", 35, "student");
-const kamal = new Person("kamal Ali", 35, "developer");
-const monir = new Person("monir Ali", 35, "teacher");
-const sojib = new Person("sojib Ali", 35, "designer");
-
-// console.log(azad);
-
-const PersonWithMethod = function (name, age, job) {
-    this.name = name;
-    this.age = age;
-    this.job = job;
-    this.dateOfBirth = function () {
-        console.log(this.name + " is born in " + (2018 - this.age));
-    };
-};
-
-const samirWithMethod = new PersonWithMethod("Samir Hossain", 22, "Web Developer");
-// samirWithMethod.dateOfBirth();
-// console.log(samirWithMethod);

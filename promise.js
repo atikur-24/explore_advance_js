eeecb87e7fd43e80e2c615d3b9d3aa2577c7d43b
atikur-24 <atikurrahman7223@gmail.com>
2024-03-19 12:41:42 +0600

    solve callback hell problem to async await methods in js

--- promise.js
+++ promise.js
@@ -1,28 +1,29 @@
+// meeting schedule with promise concept
 const hasMeeting = false;
 
 const meeting = new Promise((resolve, reject) => {
-  if (!hasMeeting) {
-    const meetingDetails = {
-      name: "Technical Meeting",
-      location: "Google Meet",
-      time: "10: 00 PM",
-    };
-    resolve(meetingDetails);
-  } else {
-    reject(new Error("Meeting already schedule!"));
-  }
+    if (!hasMeeting) {
+        const meetingDetails = {
+            name: "Technical Meeting",
+            location: "Google Meet",
+            time: "10: 00 PM",
+        };
+        resolve(meetingDetails);
+    } else {
+        reject(new Error("Meeting already schedule!"));
+    }
 });
 
 const addToCalendar = (meetingDetails) => {
-  const calendar = `${meetingDetails.name} has been scheduled on ${meetingDetails.location} at ${meetingDetails.time}`;
-  return Promise.resolve(calendar);
+    const calendar = `${meetingDetails.name} has been scheduled on ${meetingDetails.location} at ${meetingDetails.time}`;
+    return Promise.resolve(calendar);
 };
 
 meeting
-  .then(addToCalendar)
-  .then((res) => {
-    console.log(res);
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+    .then(addToCalendar)
+    .then((res) => {
+        console.log(res);
+    })
+    .catch((err) => {
+        console.log(err.message);
+    });
